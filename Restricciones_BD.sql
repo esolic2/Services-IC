faf1b4c3be3f10512c98f597c17bff265f2cd7ba
esolic2 <85380699+esolic2@users.noreply.github.com>
2021-10-27 22:26:04 -0600

    Add files via upload

---
+++ Restricciones_BD.sql
@@ -0,0 +1,18 @@
+--Ejemplo 1
+ALTER TABLE Producto
+ADD CONSTRAINT DF_Descripcion DEFAULT 'No especifica' 
+FOR Descripcion
+-----------------------------------------------------
+
+--Ejemplo 2
+ALTER TABLE Producto
+add constraint CHK_Cantidad Check(Cantidad > 10)
+
+INSERT INTO Producto (NOMBRE,DESCRIPCION,CANTIDAD) VALUES('Aspirina','Medicina',12)
+INSERT INTO Producto (NOMBRE,DESCRIPCION,CANTIDAD) VALUES('Aspirina','Medicina',45)
+
+
+
+
+
+
